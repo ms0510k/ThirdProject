4f2c390c79abd0adcc89182e3fb8feed31c3c76b
MinSung <MinSung@DESKTOP-IVJA4OP>
2018-02-20 10:17:38 +0900

    dddd

--- ThirdProject/src/kms/controller/AdminController.java
+++ ThirdProject/src/kms/controller/AdminController.java
@@ -31,7 +31,59 @@ public class AdminController extends HttpServlet{
 			notice_detail(request,response);
 		}else if(cmd.equals("delete")){
 			notice_delete(request,response);
+		}else if(cmd.equals("update")){
+			notice_update(request,response);
+		}else if(cmd.equals("updateOk")) {
+			notice_updateOk(request,response);
+		}else if(cmd.equals("search")) {
+			notice_search(request,response);
 		}
+	}
+	private void notice_search(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	String search=request.getParameter("search");
+	String word=request.getParameter("word");
+	if(search.equals("nottitle")) {
+		NoticeDao dao=new NoticeDao();
+		NoticeVo vo=dao.nottitle(word);
+		request.setAttribute("vo", vo);
+		request.getRequestDispatcher("/kms_admin/notice_search.jsp").forward(request, response);
+	}else if(search.equals("notcontent")){
+		
+	}
+	/*NoticeVo vo=new NoticeVo(notnum, nottitle, notcontent, 0, null);
+	NoticeDao dao=new NoticeDao();
+	int n=dao.updateOk(vo);
+	if(n>0) {
+		RequestDispatcher rd=request.getRequestDispatcher("/admin.do?cmd=notice");
+		rd.forward(request, response);
+	}else {
+		request.setAttribute("result","fail");
+	}*/
+	}
+	private void notice_updateOk(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	int notnum=Integer.parseInt(request.getParameter("notnum"));
+	String nottitle=request.getParameter("nottitle");
+	String notcontent=request.getParameter("notcontent");
+	NoticeVo vo=new NoticeVo(notnum, nottitle, notcontent, 0, null);
+	NoticeDao dao=new NoticeDao();
+	int n=dao.updateOk(vo);
+	if(n>0) {
+		RequestDispatcher rd=request.getRequestDispatcher("/admin.do?cmd=notice");
+		rd.forward(request, response);
+	}else {
+		request.setAttribute("result","fail");
+	}
+	}
+	private void notice_update(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	int notnum=Integer.parseInt(request.getParameter("notnum"));
+	
+	NoticeDao dao=new NoticeDao();
+	NoticeVo vo=dao.getinfo(notnum);
+	request.setAttribute("vo", vo);
+	request.getRequestDispatcher("/kms_admin/notice_update.jsp").forward(request, response);
 	}
 	private void notice_delete(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -70,9 +122,26 @@ public class AdminController extends HttpServlet{
 	}
 	private void notice_list(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String spageNum=request.getParameter("pageNum");
+		int pageNum=1;
+		if(spageNum!=null) {
+			pageNum=Integer.parseInt(spageNum);
+		}
+		int startRow=(pageNum-1)*5+1;
+		int endRow=startRow+4;
 		NoticeDao dao=new NoticeDao();
-		ArrayList<NoticeVo> list=dao.list();
+		ArrayList<NoticeVo> list=dao.list(startRow, endRow);
+		int pageCount=(int)Math.ceil(dao.getCount()/5.0);
+		int startPage=((pageNum-1)/4*4)+1;
+		int endPage=startPage+3;
+		if(pageCount<endPage) {
+			endPage=pageCount;
+		}
 		request.setAttribute("list", list);
+		request.setAttribute("pageCount", pageCount);
+		request.setAttribute("startPage", startPage);
+		request.setAttribute("endPage", endPage);
+		request.setAttribute("pageNum", pageNum);
 		request.getRequestDispatcher("/kms_admin/notice_list.jsp").forward(request, response);
 	}
 	}
